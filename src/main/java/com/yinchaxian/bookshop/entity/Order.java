97a1a0d6b5476684bd61428d3b6eaa48bc43b304
zhangfu <17770517023@163.com>
2021-07-07 16:29:07 +0800

    user and role part finished order state changge finished

---
+++ src/main/java/com/yinchaxian/bookshop/entity/Order.java
@@ -0,0 +1,150 @@
+package com.yinchaxian.bookshop.entity;
+
+import java.sql.Timestamp;
+
+public class Order {
+    private String orderId;
+    private int userId;
+    private float price;
+    private float postFee;
+    private int paymentType;
+    private int orderMount;
+    private String shippingName;
+    private String shippingCode;
+    private String buyerFeedback;
+    private int buyerRate;
+    private int status;
+    private Timestamp created;
+    private Timestamp paid;
+    private Timestamp finished;
+    private Timestamp closed;
+    private Timestamp updated;
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public float getPostFee() {
+        return postFee;
+    }
+
+    public void setPostFee(float postFee) {
+        this.postFee = postFee;
+    }
+
+    public int getPaymentType() {
+        return paymentType;
+    }
+
+    public void setPaymentType(int paymentType) {
+        this.paymentType = paymentType;
+    }
+
+    public int getOrderMount() {
+        return orderMount;
+    }
+
+    public void setOrderMount(int orderMount) {
+        this.orderMount = orderMount;
+    }
+
+    public String getShippingName() {
+        return shippingName;
+    }
+
+    public void setShippingName(String shippingName) {
+        this.shippingName = shippingName;
+    }
+
+    public String getShippingCode() {
+        return shippingCode;
+    }
+
+    public void setShippingCode(String shippingCode) {
+        this.shippingCode = shippingCode;
+    }
+
+    public String getBuyerFeedback() {
+        return buyerFeedback;
+    }
+
+    public void setBuyerFeedback(String buyerFeedback) {
+        this.buyerFeedback = buyerFeedback;
+    }
+
+    public int getBuyerRate() {
+        return buyerRate;
+    }
+
+    public void setBuyerRate(int buyerRate) {
+        this.buyerRate = buyerRate;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public Timestamp getCreated() {
+        return created;
+    }
+
+    public void setCreated(Timestamp created) {
+        this.created = created;
+    }
+
+    public Timestamp getPaid() {
+        return paid;
+    }
+
+    public void setPaid(Timestamp paid) {
+        this.paid = paid;
+    }
+
+    public Timestamp getFinished() {
+        return finished;
+    }
+
+    public void setFinished(Timestamp finished) {
+        this.finished = finished;
+    }
+
+    public Timestamp getClosed() {
+        return closed;
+    }
+
+    public void setClosed(Timestamp closed) {
+        this.closed = closed;
+    }
+
+    public Timestamp getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(Timestamp updated) {
+        this.updated = updated;
+    }
+}
---
+++ src/main/java/com/yinchaxian/bookshop/mapper/OrderMapper.java
@@ -0,0 +1,68 @@
+package com.yinchaxian.bookshop.mapper;
+
+import com.yinchaxian.bookshop.entity.Order;
+import org.apache.ibatis.annotations.*;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+public interface OrderMapper {
+    @Insert("insert into order " +
+            "values(#{orderId}, #{userId}, #{price}, #{postFee}, #{paymentType}, {orderMount}, #{shippingName}, #{shippingCode}, #{buyerFeedback}, #{buyerRate}, 0, #{created}, null, null, null, #{updated})")
+    int insert(Order order);
+
+    @Delete("delete from order " +
+            "where order_id = #{orderId} " +
+            "and user_id = #{userId}")
+    int delete(int orderId);
+
+    @Update("update order " +
+            "set price = #{price}, " +
+            "post_fee = #{postFee}, " +
+            "order_mount = #{orderMount}, " +
+            "where order_id = #{order_id}")
+    int updateInfo(Order order);
+
+
+    @Update("update order " +
+            "set payment_type = #{paymentType}, " +
+            "status = 1, " +
+            "paid = #{paid}, " +
+            "updated = #{updated} " +
+            "where order_id = #{orderId}")
+    int updatePay(Order order);
+
+    @Update("update order " +
+            "set shipping_name = #{shippingName}, " +
+            "shipping_code = #{shippingCode}, " +
+            "status = 2, " +
+            "updated = #{updated} " +
+            "where order_id = #{orderId}")
+    int updateShipping(Order order);
+
+    @Update("update order " +
+            "set status = 3, " +
+            "finished = #{finished}, " +
+            "updated = #{finished} " +
+            "where order_id = #{orderId}")
+    int updateFinish(String orderId, Timestamp finished);
+
+    @Update("update order " +
+            "set status = 4, " +
+            "closed = #{closed}, " +
+            "updated = #{closed} " +
+            "where order_id = #{orderId}")
+    int updateClose(String orderId, Timestamp closed);
+
+    @Select("select * " +
+            "from order " +
+            "where order_id = #{orderId}")
+    @ResultType(Order.class)
+    Order select(int orderId);
+
+    @Select("select * " +
+            "from order " +
+            "where user_id = #{userId}")
+    @ResultType(Order.class)
+    List<Order> selectList(int userId);
+}
